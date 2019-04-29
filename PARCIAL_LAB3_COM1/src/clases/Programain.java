f84d0158fa3dc0bd8e08e4df6e131f05599284a0
zequeMorales <ezequielmorales5@icloud.com>
2019-04-29 19:31:07 -0300

    funciones para menu
    add de funciones

--- PARCIAL_LAB3_COM1/src/clases/Programain.java
+++ PARCIAL_LAB3_COM1/src/clases/Programain.java
@@ -1,41 +1,121 @@
 package clases;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Programain {
-
+	
+	public static  Scanner teclado = new Scanner(System.in);
+	
 	public static void main(String[] args) {
+		Producto p1 =null;
+		Producto p2 =null;
+		Producto p3 =null;
+		Producto p4 =null;
+		Producto p5 =null;
+		Producto p6 =null;
+		Cliente c1=null;Cliente c2=null;Cliente c3=null; Cliente c4=null;
+		
 		
 		java.util.Date fecha = new Date();
-		System.out.println (fecha);
 		
-		
+		System.out.println("************************************");
 		VideoClub DonVideo = new VideoClub();
+		System.out.println("VIDEOCLUB DON VIDEO");
+		System.out.println("************************************");
 		
-		Cliente Carlos=new Cliente(12, "Carlos ", " Perez ");
-		DonVideo.agregarCliente(Carlos);
-		System.out.println(Carlos.mostrarCliente());
+		c1=crearCliente(DonVideo);
+		c2=crearCliente(DonVideo);
 		
-		Cliente Pedro = new Cliente(10," Pedro ", " Alvarez ");
-		DonVideo.agregarCliente(Pedro);
-		System.out.println(Pedro.mostrarCliente());
+		p1=crearProductoPelicula(DonVideo);
 		
-		VideoJuego CallOfDuty = new VideoJuego(1, "Call Of Duty", "Shooter", 10, "Unisoft", 10);
-		DonVideo.agregarProducto(CallOfDuty);
-		System.out.println(CallOfDuty.imprimir());
+		p6=crearProductoVideojuego(DonVideo);
 		
-		//entrego el juego y agrego reg
-		CallOfDuty.entregar();
-		CallOfDuty.agregarRegistroAlqJuego(fecha, Carlos);
+		System.out.println("INGRESE LA CANTIDAD DE COPIAS");
+		int cantCopias;
+		cantCopias=teclado.nextInt();
+		DonVideo.agregarCopias(p1, cantCopias);
+		
 
-		System.out.println(CallOfDuty.imprimir());
+	
+		System.out.println("************************************");
 		
-		CallOfDuty.listarRegistro();
+		DonVideo.listarClientes();
+		System.out.println("************************************");
+		
+		DonVideo.eliminarCliente(10);
+		System.out.println("************************************");
+		DonVideo.listarClientes();
 		
 		
+
+		teclado.close();
+	}
+	public static Producto crearProductoPelicula(VideoClub donv){
+		int id;
+		String titulo;
+		String genero;
+		String creador;
+		int duracion;
 		
+		System.out.println("Ingrese el id de la Pelicula");
+		id=teclado.nextInt();
+		System.out.println("Ingrese el Titulo de la Pelicula");
+		titulo=teclado.next();
+		System.out.println("Ingrese el Genero de la Pelicula");
+		genero=teclado.next();
+		System.out.println("Ingrese la Creador de la Pelicula");
+		creador=teclado.next();
+		System.out.println("Ingrese la Duracion de la Pelicula");
+		duracion=teclado.nextInt();
+		Producto p = new Pelicula(id, titulo, genero, duracion, creador);
+		donv.agregarProducto(p);
+		System.out.println(p.imprimir());
+		return p;
+
+	}
+	
+	public static Producto crearProductoVideojuego(VideoClub donv){
+		int id;
+		String titulo;
+		String genero;
+		String compania;
+		int horasEstimadas;
 		
+		System.out.println("Ingrese el id del Videojuego");
+		id=teclado.nextInt();
+		System.out.println("Ingrese el Titulo del Videojuego");
+		titulo=teclado.next();
+		System.out.println("Ingrese el Genero del Videojuego");
+		genero=teclado.next();
+		System.out.println("Ingrese la Compañia del Videojuego");
+		compania=teclado.next();
+		System.out.println("Ingrese las Horas estimadas del VideoJuego");
+		horasEstimadas=teclado.nextInt();
+		Producto p = new VideoJuego(id, titulo, genero, compania, horasEstimadas);
+		donv.agregarProducto(p);
+		System.out.println(p.imprimir());
+		return p;
+		}
+	public static Cliente crearCliente(VideoClub donv){
+		int id;
+		String Nombre;
+		String Apellido;
+		
+		System.out.println("Ingrese el id del Cliente");
+		id=teclado.nextInt();
+		System.out.println("Ingrese el Nombre del Cliente");
+		Nombre=teclado.next();
+		System.out.println("Ingrese el Apellido del Cliente");
+		Apellido=teclado.next();
+		Cliente c = new Cliente(id, Nombre, Apellido);
+		donv.agregarCliente(c);
+		System.out.println(c.mostrarCliente());
+		return c;
 		
 	}
+
+
+	
 
 }
